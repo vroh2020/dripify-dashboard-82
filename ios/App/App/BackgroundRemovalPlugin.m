a41ef626b4f7eb96867cc4fb0fd1768ecc1db87d
Rohini <vroh2021@gmail.com>
2025-11-30 15:29:04 -0500

    Update Xcode project iOS deployment target to 17.0 for all build configurations

--- ios/App/App/BackgroundRemovalPlugin.m
+++ ios/App/App/BackgroundRemovalPlugin.m
@@ -1,11 +1,6 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
 
-// Import the Swift bridging header to expose Swift class to Obj-C
-#if __has_include("App-Swift.h")
-#import "App-Swift.h"
-#endif
-
 CAP_PLUGIN(BackgroundRemovalPlugin, "BackgroundRemoval",
     CAP_PLUGIN_METHOD(removeBackground, CAPPluginReturnPromise);
 )
